c0827e6bbb34c317c2b84a68efab8c8c03d6bcf5
Jishnnu <jishnuanil30@gmail.com>
2023-08-01 16:49:41 +0530

    Initial Commit

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-import base64
 import torch
 import streamlit as st
 from transformers import AutoTokenizer, AutoModelForSeq2SeqLM, pipeline
@@ -12,7 +11,9 @@ checkpoint = "LaMini-Flan-T5-783M"
 tokenizer = AutoTokenizer.from_pretrained(checkpoint)
 base_model = AutoModelForSeq2SeqLM.from_pretrained(
     checkpoint,
-    device_map="auto",   # Check device configurations for better computation
+    device_map="auto",
+    offload_folder="offload",
+    offload_state_dict = True,
     torch_dtype=torch.float32
 )
 
@@ -30,3 +31,46 @@ def llm_pipeline():
 
     local_llm = HuggingFacePipeline(pipeline=pipeline)
     return local_llm
+
+@st.cache_resource
+def llm_qa():
+    llm = llm_pipeline()
+    embeddings = SentenceTransformerEmbeddings(model_name="all-MiniLM-L6-v2")
+    db = Chroma(persist_directory="Database", embedding_function=embeddings, client_settings=CHROMA_SETTINGS)
+    
+    # Infer embeddings
+    retriever = db.as_retriever()
+    qa_retriever=RetrievalQA.from_chain_type(
+        llm=llm,
+        chain_type="stuff",
+        retriever=retriever,
+        return_source_documents=True
+    )
+
+    return qa_retriever
+
+def process_answers(instructions):
+    response = ''        
+    generated_text = llm_qa(instructions)
+    answer = generated_text['result']
+    return answer, generated_text
+
+def main():
+    st.title('DocSense')
+    with st.expander("About"):
+        st.markdown(
+            """
+            Welcome to AI DocSense, your go-to web app for expertly answering AI-related questions. Ask us anything, and we'll provide expert answers to your AI-related queries, making complex concepts simple. Empowering knowledge through intuitive interactions with Generative AI.
+            """
+        )
+    
+    question = st.text_area("Enter your Queries")
+    if st.button("Search"):
+        st.info("QUESTION: " + question)
+        st.info("ANSWER: ")
+        answer, metadata = process_answers(question)
+        st.write(metadata)
+        st.write(answer)
+
+if __name__ == "__main__":
+    main()
